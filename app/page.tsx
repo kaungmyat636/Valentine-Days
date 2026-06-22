c9d2066d2b35f29fb554a0fc15fa4592cd6b6514
kaungmyat636 <km6355183@gmail.com>
2026-06-22 11:29:46 +0630

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,21 +8,11 @@ export default function Home() {
   const [showGallery, setShowGallery] = useState(false);
 
   const userImages = [
-    '/1.jpeg',
-    '/2.jpeg',
-    '/3.jpeg',
-    '/4.jpeg',
-    '/5.jpeg',
-    '/6.jpeg',
-    '/7.jpeg',
-    '/8.jpeg',
-    '/9.jpeg',
-    '/10.jpeg',
-    '/11.jpeg',
-    '/12.jpeg',
-    '/13.jpeg',
-    '/14.jpeg',
-    '/15.jpeg',
+    '/1.jpg[1].png',
+    '/2.jpg[1].jpeg',
+    '/3.jpg[1].png',
+    '/4.jpg[1].jpeg',
+    '/5.jpg[1].jpg',
   ];
 
   return (
